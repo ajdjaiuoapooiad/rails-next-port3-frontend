93041009891d17ccf9dbba8dbabdb92193fab38f
yukito <iyggf66974@yahoo.ne.jp>
2025-04-29 13:50:10 +0900

    indexPage icon追加

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -54,22 +54,17 @@ export default async function PostIndexPage() {
                   <div className="flex justify-between text-gray-500 text-sm">
                     <button className="flex items-center space-x-1 hover:text-blue-500 focus:outline-none">
                       <ChatBubbleLeftIcon className="h-5 w-5" />
-                      <span>コメント</span>
                     </button>
                     <button className="flex items-center space-x-1 hover:text-green-500 focus:outline-none">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
                       </svg>
-
-                      <span>リツイート</span>
                     </button>
                     <button className="flex items-center space-x-1 hover:text-red-500 focus:outline-none">
                       <HeartIcon className="h-5 w-5" />
-                      <span>いいね</span>
                     </button>
                     <button className="flex items-center space-x-1 hover:text-gray-700 focus:outline-none">
                       <ShareIcon className="h-5 w-5" />
-                      <span>シェア</span>
                     </button>
                     <button className="hover:text-gray-700 focus:outline-none">
                       <EllipsisHorizontalIcon className="h-5 w-5" />
