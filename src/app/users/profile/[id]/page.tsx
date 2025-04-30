ac4e1de97b2938ea15e97f800b96df737346b7ff
yukito <iyggf66974@yahoo.ne.jp>
2025-04-30 11:27:05 +0900

    profilePage navbar追加

--- src/app/users/profile/[id]/page.tsx
+++ src/app/users/profile/[id]/page.tsx
@@ -76,8 +76,8 @@ const ProfilePage: React.FC = () => {
           </div>
 
           {/* ナビゲーションバー */}
-          <div className="mt-6 border-b border-gray-200">
-            <nav className="-mb-px flex space-x-4" aria-label="Tabs">
+          <div className="mt-6 border-b border-gray-200 w-full flex justify-center">
+            <nav className="-mb-px flex space-x-4 max-w-3xl mx-auto" aria-label="Tabs">
               <button
                 onClick={() => setActiveTab('posts')}
                 className={`whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm ${
@@ -86,6 +86,7 @@ const ProfilePage: React.FC = () => {
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:border-gray-400'
                 }`}
               >
+
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5 mr-1 inline-block">
                   <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clipRule="evenodd" />
                 </svg>
