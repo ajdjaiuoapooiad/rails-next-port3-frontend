6760aec7cf61846403519124e602d4327a69edad
yukito <iyggf66974@yahoo.ne.jp>
2025-04-30 15:31:02 +0900

    終生

--- src/app/components/posts/PostList.tsx
+++ src/app/components/posts/PostList.tsx
@@ -30,7 +30,7 @@ const PostList: React.FC = () => {
       setLoading(true);
       setError(null);
       try {
-        const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/posts', {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
           cache: 'no-store', // キャッシュを無効にする（必要に応じて変更）
         });
         if (!res.ok) {
